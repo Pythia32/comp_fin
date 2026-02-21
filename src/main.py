1f2ac8058ddaf1c18517ea88512368b42199cb1e
Pythia32 <michaeldevilee@gmail.com>
2026-02-21 11:46:43 +0100

    Cleaned up main.py, and added code for step 3. Have moved the old code of main.py (before cleaning) to a new file aptly named backup_main.py

--- src/main.py
+++ src/main.py
@@ -11,202 +11,146 @@ from yahooquery import Ticker
 
 from utils import fetch_spx_close, fetch_vix_close
 
+
 #### Step 1:
-## Data Loading
-data_path = "data/" + "option20230201_20230228.csv"
-data_SPX = pd.read_csv(data_path) # Load SPX option quote data
-data_SPX = data_SPX[["date","exdate","symbol","strike_price","best_bid","best_offer","impl_volatility","delta","cp_flag"]] # Discard irrelevant features
-print("Raw data observation count: ", data_SPX.shape[0]) # Reporting..
+## Load SPX (call) option quote dataset
+fp = "data/" + "option20230201_20230228.csv" # File path
+SPX_data = pd.read_csv(fp)
+SPX_data = SPX_data[["date","exdate","symbol","strike_price","best_bid","best_offer","impl_volatility","delta","cp_flag"]] # Keep only relevant features
 
 
-## Data Preprocessing
-calls = data_SPX[data_SPX["cp_flag"] == "C"].copy() # Keep calls only
-print("Observation count after filtering to calls: ", calls.shape[0]) # Reporting..
+## Preprocessing
+SPX_calls = SPX_data[SPX_data["cp_flag"] == "C"].copy() # Keep calls only
 
-calls["date"] = pd.to_datetime(calls["date"]) # Convert entry type for "date features" to datetime64[ns] for consistency
-calls["exdate"] = pd.to_datetime(calls["exdate"])
-calls["strike"] = calls["strike_price"] / 1000 # Rescale strikes to match index-point units, i.e. division by 1000
-calls.drop(columns="strike_price", inplace=True)
-calls["midquote"] = (calls["best_bid"] + calls["best_offer"]) / 2 # Construct midquote
-# print("\nMidquote (V_t) missing rate: ", calls["midquote"].isna().mean())
+SPX_calls["date"] = pd.to_datetime(SPX_calls["date"]) # Consistent data type for temporal features
+SPX_calls["exdate"] = pd.to_datetime(SPX_calls["exdate"])
+SPX_calls["strike"] = SPX_calls["strike_price"] / 1000 # Rescale strikes to match index-point units
+SPX_calls.drop(columns="strike_price", inplace=True)
+SPX_calls["midquote"] = (SPX_calls["best_bid"] + SPX_calls["best_offer"]) / 2 # Construct midquote
+
+SPX_calls_no_nan = SPX_calls.dropna(subset=["impl_volatility"]) # Remove observations with missing (NaN) or invalid (<=0) implied volatility
+SPX_calls_no_nan = SPX_calls_no_nan[SPX_calls_no_nan["impl_volatility"] > 0]
+
+
+## Reporting (1)
+print("Raw observation count: ", SPX_data.shape[0])
+
+print("Observation count after filtering to calls: ", SPX_calls.shape[0])
 print("\nMidquote (V_t) missing / incorrect data rate: ",
   (
-      calls["best_bid"].isna() | # Missing entries
-      calls["best_offer"].isna() |
-      (calls["best_bid"] < 0) | # Best bid may be 0 (no buyers), but should never be negative
-      (calls["best_offer"] < 0) | # Best offer should always be positive, as the value of a European call option is always at least 0, and giving away such an option for free is not sensible
-      (calls["best_bid"] >= calls["best_offer"]) # This should result in a market order, clearing the associated bid/offer
+      SPX_calls["best_bid"].isna() | # Missing entries
+      SPX_calls["best_offer"].isna() |
+      (SPX_calls["best_bid"] < 0) | # Best bid may be 0 (no buyers), but should never be negative
+      (SPX_calls["best_offer"] < 0) | # Best offer should always be positive, as the value of a European call option is always at least 0, and giving away such an option for free is not sensible
+      (SPX_calls["best_bid"] >= SPX_calls["best_offer"]) # This should result in a market order, clearing the associated bid/offer
   ).mean())
-print("Implied volatility (sigma_mkt) missing / incorrect data rate: ", (calls["impl_volatility"].isna() | (calls["impl_volatility"] <= 0)).mean())
-print("Delta missing data rate: ", calls["delta"].isna().mean())
+print("Implied volatility (sigma_mkt) missing / incorrect data rate: ", (SPX_calls["impl_volatility"].isna() | (SPX_calls["impl_volatility"] <= 0)).mean())
+print("Delta missing data rate: ", SPX_calls["delta"].isna().mean())
 
-calls = calls.dropna(subset=["impl_volatility"]) # Remove observations with missing (NaN) or invalid (<=0) implied volatility
-calls = calls[calls["impl_volatility"] > 0]
-print("\nFinal observation count after preprocessing: ", calls.shape[0]) # Reporting..
-print("\nUnique trading dates (after preprocessing): ", calls["date"].nunique())
-print("Unique expiries (after preprocessing): ", calls["exdate"].nunique())
-
-print("\nFinal columns: ", calls.columns)
+print("\nFinal observation count after preprocessing: ", SPX_calls_no_nan.shape[0])
+print("\nUnique trading dates (after preprocessing): ", SPX_calls_no_nan["date"].nunique())
+print("Unique expiries (after preprocessing): ", SPX_calls_no_nan["exdate"].nunique())
+print("\nFinal columns: ", SPX_calls_no_nan.columns)
 
 
 ## Further Data Augmenting..
 # spx_close = fetch_spx_close("2023-02-01", "2023-03-01")
+# print(spx_close)
 
 
-#### Step 2:
-## Construct Contract Time Series
-contracts = calls['symbol'].unique().tolist()
-# print(type(contracts))
-# print(contracts)
 
-dates = calls["date"].unique().tolist()
-# print(type(dates[0]))
-# print(dates)
+#### Step 2
+contracts = SPX_calls_no_nan['symbol'].unique().tolist()
+dates = SPX_calls_no_nan["date"].unique().tolist()
 
-dates_start = dates[:-1:]
-dates_end = dates[1::]
-date_delta_pairs = list(zip(dates_start, dates_end))
+start_dates = dates[:-1:]
+end_dates = dates[1::]
+sequential_date_pairs = list(zip(start_dates, end_dates))
 
-print("Date missing data rate: ", calls["date"].isna().mean())  # No dates missing! :)
-
-# print(dates)
-# print(dates[:-1:])
-
-# c = contracts[0]
-# d = dates[0]
-# test = calls[
-#       (calls["symbol"]==c) & 
-#       (calls["date"]==d)
-#       ]["midquote"] #.item()
-# print(test)
-# print(type(test))
-# print(test.size)
-
-### Step 2/ attempt 4:  SUCCESS + FAST!!
-contract_ts_df = (  # DataFrame of V_t, where each row corresponds to a unique contract ("symbol"), and each column corresponds to a unique date ("date")
-    calls
+## Construct DataFrame containing the midquote time series for each contract (row) and across all dates (column) in the preprocessed dataset.
+SPX_call_midquote_series = (  # DataFrame of V_t, where each row corresponds to a unique contract ("symbol"), and each column corresponds to a unique date ("date")
+    SPX_calls_no_nan
     .pivot(index="symbol", columns="date", values="midquote")
     .reindex(index=contracts)
 )
-contract_delta_ts = []
-for (start, end) in date_delta_pairs:
-    delta_v_vec = contract_ts_df[end] - contract_ts_df[start]
-    contract_delta_ts.append(delta_v_vec)
 
-contract_delta_ts_df = pd.concat(contract_delta_ts, axis=1)  # DataFrame of delta V_t = V_{t+1} - V_t, where each row corresponds to a unique contract ("symbol"), and each column corresponds to a unique pair (start_date, end_date), specifying the location of the time-increment where the difference (delta) was computed.
-print(contract_delta_ts_df)
-# print(contract_ts_df)
+SPX_call_midquote_series.columns.name = "date"
+SPX_call_midquote_series.index.name = "contract (symbol)"
+
+SPX_call_midquote_series_no_nan = SPX_call_midquote_series.dropna() # Remove observations (contracts) with incomplete time series (having NaN entries)
+
+## Construct DataFrame containing the midquote delta (sequential difference) time series for each contract (row) and across all dates (column) in the preprocessed dataset.
+delta_series_data = []
+for (start, end) in sequential_date_pairs:
+    deltas_at_time_t = SPX_call_midquote_series[end] - SPX_call_midquote_series[start]
+    delta_series_data.append(deltas_at_time_t)
+SPX_call_delta_series = pd.concat(delta_series_data, axis=1)  # DataFrame of delta V_t = V_{t+1} - V_t, where each row corresponds to a unique contract ("symbol"), and each column corresponds to a unique pair (start_date, end_date), specifying the location of the time-increment where the difference (delta) was computed.
+
+SPX_call_delta_series.columns = start_dates # Rename columns to match the corresponding dates
+SPX_call_delta_series.columns.name = "date"
+SPX_call_delta_series.index.name = "contract (symbol)"
+
+SPX_call_delta_series_no_nan = SPX_call_delta_series.dropna() # Remove observations (contracts) with incomplete time series (having NaN entries)
+
+# Consistency check: number of observations (rows) in midquote time series df and in midquote delta time series df should be equal
+if (SPX_call_midquote_series_no_nan.shape[0] != SPX_call_delta_series_no_nan.shape[0]):
+    raise ValueError(
+        f"\nConsistency check failed: "
+        f"SPX_call_midquote_series_no_nan has {SPX_call_midquote_series_no_nan.shape[0]} rows,"
+        f"SPX_call_delta_series_no_nan has {SPX_call_delta_series_no_nan.shape[0]} rows. These should be equal."
+    )
 
 
-## Reporting
+## Reporting (2)
 # Summary statistics of V_t and delta V_t
 
 # Number of contracts retained(?) after constructing delta V_t
-contract_delta_ts_df_no_nan = contract_delta_ts_df.dropna()
-print("/nNumber of contracts after preprocessing: ", contract_delta_ts_df.shape[0])
-print("Number of contracts retained after constructing delta V_t: ", contract_delta_ts_df_no_nan.shape[0])
-
-### Check consistency for number of rows containing NaN's between V_t and delta V_t
-contract_ts_df_no_nan = contract_ts_df.dropna()
-print(contract_ts_df_no_nan.shape[0])  # == 4975, hence the number of rows containing NaN entries in V_t is consistent with that in delta V_t.
-
-### --> Question: How many contracts do not exist before some date d; but have otherwise complete entries for dates' remainder?
-columns = list(contract_delta_ts_df.columns)
-# print(columns)
-for i in columns:
-    test_df = contract_delta_ts_df.iloc[:, i:].dropna()
-    print(f"Number of rows retained including columns starting from {i}: {test_df.shape[0]}") # Number of rows retained is strictly increasing as i increases. Ultimately we would retain a total 7841 rows if we were to retain also entries for which at least one delta V_t entry is not NaN and all subsequent entries (increasing columns) are also not NaN.
-
-## // Legacy Code for Step 2: //
+print("\nNumber of unique contracts in the preprocessed dataset: ", SPX_call_delta_series.shape[0])
+print("Number of contracts retained (complete time series) after constructing delta V_t: ", SPX_call_delta_series_no_nan.shape[0]) # Note: retained contracts have a complete time series, discarded contracts do not
 
 
-### Step 2/ attempt 3:  SUCCESS (But SLOW)
-# calls_indexed = calls.set_index(["symbol", "date"])["midquote"]
-# contract_ts_list = []
-# for c in contracts:
-#   row = []
-#   for (start, end) in date_delta_pairs:
-#       v_start = calls_indexed.get((c, start))
-#       v_end   = calls_indexed.get((c, end))
-#       if v_start is None or v_end is None:
-#           row.append(np.nan)
-#       elif isinstance(v_start, float) and isinstance(v_end, float):
-#           delta_v = v_end - v_start
-#           row.append(delta_v)
-#           # print(v_start.size) ###
-#           # print(v_start.item()) ###
-#       else:
-#         raise TypeError("Local variables v_start and v_end should be floats (or boolean with value None)")
-#   contract_ts_list.append(row)
-# print(contract_ts_list)
 
-### attempt 3/ Testing:
-# c = contracts[100]
-# print(c)
-# test_data = calls[calls["symbol"]==c]["date"]
-# print(test_data)
-# # print(dates[11])
-# d_early = dates[10]
-# d_late = dates[11]
-# test1 = calls_indexed.get((c,d_early))
-# print(test1)  # Should(!) return a float
-# test2 = calls_indexed.get((c,d_late))
-# print(test2) # Returns None (because searched entry index does not exist)
-# #//
-# (start, end) = date_delta_pairs[10]
-# print(start, end)
+#### Step 3:
+## SPX close series
+start = "2023-02-01"
+end = "2023-03-02"
 
-### Step 2/ attempt 2:  FAIL
-# contract_ts_list = []
-# for c in contracts:
-#   row = []
-#   for (start, end) in date_delta_pairs:
-#     try:
-#       delta_V_t = calls[
-#         (calls["symbol"]==c) & 
-#         (calls["date"]==end)
-#         ]["midquote"].item() - calls[
-#         (calls["symbol"]==c) & 
-#         (calls["date"]==start)
-#         ]["midquote"].item()
-#       print(delta_V_t) ###
-#       row.append(delta_V_t)
-#     except ValueError as e:
-#       if "can only convert an array of size 1 to a Python scalar" in str(e):
-#         print("Missing entry detected, substituting NaN entry")
-#         row.append(np.nan)
-#       else:
-#         raise # Raise if unexpected ValueError
-#     except Exception as e: # Catches and identifies any potential exceptions not yet observed.
-#       print("Exception was: ", e)
-#       print(type(e))
-#   contract_ts_list.append(row)
-# print(contract_ts_list)
+d1 = pd.Timestamp(start).strftime("%Y%m%d")
+d2 = pd.Timestamp(end).strftime("%Y%m%d")
 
-### Step 2/ attempt 1:  FAIL
-# contract_ts_list = []
-# for c in contracts:
-#   row = []
-#   for (start, end) in date_delta_pairs:
-#     delta_V_t = calls[
-#       (calls["symbol"]==c) & 
-#       (calls["date"]==end)
-#       ]["midquote"] - calls[
-#       (calls["symbol"]==c) & 
-#       (calls["date"]==start)
-#       ]["midquote"]
-#     if delta_V_t.size==1:
-#       print(delta_V_t.item())
-#       row.append(delta_V_t.item())
-#     elif delta_V_t.size==0:
-#       print("Missing entry")
-#       row.append(np.nan)
-#     elif delta_V_t.size>=2:
-#       # print("ERROR: too many entries")
-#       raise ValueError(f"Multiple entries found for symbol={c}, date={d}")
-#     else:
-#       raise ValueError(f"Incompatible data found for symbol={c}, date={d}")
-#   contract_ts_list.append(row)
+url = f"https://stooq.com/q/d/l/?s=^spx&d1={d1}&d2={d2}&i=d"
+df = pd.read_csv(url, parse_dates=["Date"]).sort_values("Date")
+
+SPX_price_series = df.set_index("Date")["Close"]
+print(SPX_price_series)
+# print(type(SPX_price_series))
+
+# print(SPX_call_midquote_series_no_nan)
 
 
-# contract_ts_df = pd.DataFrame(contract_ts_list)
+## Delta SPX close series
+next = SPX_price_series.shift(-1)
+# print(next)
+delta_SPX_price_series = (next - SPX_price_series).dropna()
+print(delta_SPX_price_series)
+
+
+## Reporting
+# Merge coverage with (V_t/ delta V_t)
+# ... TO DO!
+
+# Summary statistics
+print(SPX_price_series.describe())
+print(delta_SPX_price_series.describe())
+
+# Plots of S_t and delta S_t over time
+SPX_price_series.plot(title="SPX Close Price")
+# plt.show()
+
+delta_SPX_price_series.plot(title="Daily SPX Change")
+# plt.show()
+
+print(dates)
+print(SPX_call_midquote_series_no_nan)
+print(SPX_call_delta_series_no_nan)
+print(type(SPX_call_midquote_series_no_nan))
