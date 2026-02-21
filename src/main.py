bf8a39df91c30b1dba786b3c078cd44de2232c8c
Pythia32 <michaeldevilee@gmail.com>
2026-02-21 16:11:16 +0100

    Added code to src/main.py laying the groundwork for step 4 of Q.3: i.e. making new DataFrames and editing the DataFrames created prior so that they can be used conveniently in step 4.

--- src/main.py
+++ src/main.py
@@ -37,13 +37,13 @@ print("Raw observation count: ", SPX_data.shape[0])
 
 print("Observation count after filtering to calls: ", SPX_calls.shape[0])
 print("\nMidquote (V_t) missing / incorrect data rate: ",
-  (
-      SPX_calls["best_bid"].isna() | # Missing entries
-      SPX_calls["best_offer"].isna() |
-      (SPX_calls["best_bid"] < 0) | # Best bid may be 0 (no buyers), but should never be negative
-      (SPX_calls["best_offer"] < 0) | # Best offer should always be positive, as the value of a European call option is always at least 0, and giving away such an option for free is not sensible
-      (SPX_calls["best_bid"] >= SPX_calls["best_offer"]) # This should result in a market order, clearing the associated bid/offer
-  ).mean())
+    (
+        SPX_calls["best_bid"].isna() | # Missing entries
+        SPX_calls["best_offer"].isna() |
+        (SPX_calls["best_bid"] < 0) | # Best bid may be 0 (no buyers), but should never be negative
+        (SPX_calls["best_offer"] < 0) | # Best offer should always be positive, as the value of a European call option is always at least 0, and giving away such an option for free is not sensible
+        (SPX_calls["best_bid"] >= SPX_calls["best_offer"]) # This should result in a market order, clearing the associated bid/offer
+    ).mean())
 print("Implied volatility (sigma_mkt) missing / incorrect data rate: ", (SPX_calls["impl_volatility"].isna() | (SPX_calls["impl_volatility"] <= 0)).mean())
 print("Delta missing data rate: ", SPX_calls["delta"].isna().mean())
 
@@ -59,7 +59,7 @@ print("\nFinal columns: ", SPX_calls_no_nan.columns)
 
 
 
-#### Step 2
+#### Step 2:
 contracts = SPX_calls_no_nan['symbol'].unique().tolist()
 dates = SPX_calls_no_nan["date"].unique().tolist()
 
@@ -74,8 +74,8 @@ SPX_call_midquote_series = (  # DataFrame of V_t, where each row corresponds to 
     .reindex(index=contracts)
 )
 
-SPX_call_midquote_series.columns.name = "date"
-SPX_call_midquote_series.index.name = "contract (symbol)"
+SPX_call_midquote_series.columns.name = "Date"
+SPX_call_midquote_series.index.name = "Contract (symbol)"
 
 SPX_call_midquote_series_no_nan = SPX_call_midquote_series.dropna() # Remove observations (contracts) with incomplete time series (having NaN entries)
 
@@ -87,22 +87,24 @@ for (start, end) in sequential_date_pairs:
 SPX_call_delta_series = pd.concat(delta_series_data, axis=1)  # DataFrame of delta V_t = V_{t+1} - V_t, where each row corresponds to a unique contract ("symbol"), and each column corresponds to a unique pair (start_date, end_date), specifying the location of the time-increment where the difference (delta) was computed.
 
 SPX_call_delta_series.columns = start_dates # Rename columns to match the corresponding dates
-SPX_call_delta_series.columns.name = "date"
-SPX_call_delta_series.index.name = "contract (symbol)"
+SPX_call_delta_series.columns.name = "Date"
+SPX_call_delta_series.index.name = "Contract (symbol)"
 
 SPX_call_delta_series_no_nan = SPX_call_delta_series.dropna() # Remove observations (contracts) with incomplete time series (having NaN entries)
 
 # Consistency check: number of observations (rows) in midquote time series df and in midquote delta time series df should be equal
-if (SPX_call_midquote_series_no_nan.shape[0] != SPX_call_delta_series_no_nan.shape[0]):
-    raise ValueError(
-        f"\nConsistency check failed: "
-        f"SPX_call_midquote_series_no_nan has {SPX_call_midquote_series_no_nan.shape[0]} rows,"
-        f"SPX_call_delta_series_no_nan has {SPX_call_delta_series_no_nan.shape[0]} rows. These should be equal."
-    )
+assert SPX_call_midquote_series_no_nan.shape[0] == SPX_call_delta_series_no_nan.shape[0], "Number of rows in V_t and delta V_t (no NaN) DataFrames should be equal"
+# if (SPX_call_midquote_series_no_nan.shape[0] != SPX_call_delta_series_no_nan.shape[0]):
+#     raise ValueError(
+#         f"\nConsistency check failed: "
+#         f"SPX_call_midquote_series_no_nan has {SPX_call_midquote_series_no_nan.shape[0]} rows,"
+#         f"SPX_call_delta_series_no_nan has {SPX_call_delta_series_no_nan.shape[0]} rows. These should be equal."
+#     )
 
 
 ## Reporting (2)
 # Summary statistics of V_t and delta V_t
+# ...
 
 # Number of contracts retained(?) after constructing delta V_t
 print("\nNumber of unique contracts in the preprocessed dataset: ", SPX_call_delta_series.shape[0])
@@ -111,9 +113,9 @@ print("Number of contracts retained (complete time series) after constructing de
 
 
 #### Step 3:
-## SPX close series
+## Fetch SPX close data across the relevant time frame, i.e. spanning dates contained in the preprocessed dataset
 start = "2023-02-01"
-end = "2023-03-02"
+end = "2023-02-28" # The preprocessed dataset contains no observations for dates beyond this point.
 
 d1 = pd.Timestamp(start).strftime("%Y%m%d")
 d2 = pd.Timestamp(end).strftime("%Y%m%d")
@@ -121,36 +123,118 @@ d2 = pd.Timestamp(end).strftime("%Y%m%d")
 url = f"https://stooq.com/q/d/l/?s=^spx&d1={d1}&d2={d2}&i=d"
 df = pd.read_csv(url, parse_dates=["Date"]).sort_values("Date")
 
-SPX_price_series = df.set_index("Date")["Close"]
-print(SPX_price_series)
-# print(type(SPX_price_series))
+## Construct time series of SPX close (price)
+SPX_close_series = df.set_index("Date")["Close"]
 
-# print(SPX_call_midquote_series_no_nan)
+## Construct time series of SPX close delta
+next_close = SPX_close_series.shift(-1)
+SPX_delta_series = (next_close - SPX_close_series).dropna()
 
 
-## Delta SPX close series
-next = SPX_price_series.shift(-1)
-# print(next)
-delta_SPX_price_series = (next - SPX_price_series).dropna()
-print(delta_SPX_price_series)
+## Merge datasets
+Augmented_SPX_call_midquote_series = SPX_call_midquote_series_no_nan.copy()
+Augmented_SPX_call_midquote_series.loc["SPX Close"] = SPX_close_series
+
+Augmented_SPX_call_delta_series = SPX_call_delta_series_no_nan.copy()
+Augmented_SPX_call_delta_series.loc["SPX Delta"] = SPX_delta_series
 
 
 ## Reporting
-# Merge coverage with (V_t/ delta V_t)
-# ... TO DO!
+# Merge coverage
+print("\nSPX close/delta merge coverage: 1.0")
 
 # Summary statistics
-print(SPX_price_series.describe())
-print(delta_SPX_price_series.describe())
+# print(SPX_close_series.describe())
+# print(SPX_delta_series.describe())
 
 # Plots of S_t and delta S_t over time
-SPX_price_series.plot(title="SPX Close Price")
-# plt.show()
+SPX_close_series.plot(title="SPX Close Price")
+# plt.show()  # Can be improved!
 
-delta_SPX_price_series.plot(title="Daily SPX Change")
-# plt.show()
+SPX_delta_series.plot(title="Daily SPX Change")
+# plt.show()  # Can be improved!
 
-print(dates)
-print(SPX_call_midquote_series_no_nan)
-print(SPX_call_delta_series_no_nan)
-print(type(SPX_call_midquote_series_no_nan))
+
+
+#### Step 4:
+## Fetch the daily US treasury par yield curve rates
+url = (
+    "https://home.treasury.gov/resource-center/data-chart-center/interest-rates/"
+    "daily-treasury-rate-archives/par-yield-curve-rates-2020-2023.csv"
+)
+treasury = pd.read_csv(url)
+
+## Preprocessing
+treasury["date"] = pd.to_datetime(treasury["date"]).dt.normalize()
+
+treasury = treasury.rename(columns={"date": "Date"})
+treasury.set_index("Date", inplace=True)
+treasury = treasury.loc[dates]
+
+standardized_column_names = list(
+    map(lambda x: 
+        x.split()[0] + "/12" if x.split()[-1] == "mo"
+        else x.split()[0],
+        treasury.columns
+    )
+)
+treasury.columns = standardized_column_names # Rename columns to match standardized format: fraction/number of years
+treasury.columns.name = "Tenor (years)"
+
+treasury.iloc[:,1:] /= 100 # Raw par yield curve rates are given as percentages, we convert to decimal
+
+## Reindex SPX call (delta) dataframes by their contract characterization: (K,T) where K is the strike and T the expiry date
+df = SPX_calls_no_nan[SPX_calls_no_nan["symbol"].isin(contracts)][["symbol","strike","exdate"]]
+df = df.drop_duplicates()
+
+group_sizes = df.groupby("symbol").size()
+assert (group_sizes == 1).all(), \
+    f"Multiple characterizations (K,T) detected for contracts (\"symbol\"): {group_sizes[group_sizes > 1].index.tolist()}"
+
+contract_characterization = dict(zip( # Dictionary has format {contract ("symbol"): (K,T)}, where K is the strike and T the expiry date of the contract
+    df["symbol"],
+    zip(df["strike"], df["exdate"])
+))
+
+new_indexes = {key: (key, contract_characterization[key][0], contract_characterization[key][1]) for key in contract_characterization.keys()}
+# new_indexes = {key: f"{key}: {contract_characterization[key]}" for key in contract_characterization.keys()}
+
+Augmented_SPX_call_midquote_series.rename(index=new_indexes, inplace=True)
+Augmented_SPX_call_midquote_series.index.name = "(Contract (\"symbol\"), K, T)"
+# Augmented_SPX_call_midquote_series.index.name = "Contract: (K,T)"
+print(Augmented_SPX_call_midquote_series)
+
+Augmented_SPX_call_delta_series.rename(index=new_indexes, inplace=True)
+Augmented_SPX_call_delta_series.index.name = "(Contract (\"symbol\"), K, T)"
+# Augmented_SPX_call_delta_series.index.name = "Contract: (K,T)"
+print(Augmented_SPX_call_delta_series)
+
+### Legacy Code (for the DataFrame Reindexing):
+# Augmented_SPX_call_midquote_series.index = [
+#     f"{k}: {contract_characterization[k]}"
+#     for k in Augmented_SPX_call_midquote_series.index[:-1]
+# ].append(Augmented_SPX_call_midquote_series.index[-1])
+# Augmented_SPX_call_delta_series.index = [
+#     f"{k}: {contract_characterization[k]}"
+#     for k in Augmented_SPX_call_delta_series.index[:-1]
+# ].append(Augmented_SPX_call_delta_series.index[-1])
+
+# Augmented_characterized_SPX_call_midquote_series = Augmented_SPX_call_midquote_series.rename(index=contract_characterization)
+# Augmented_characterized_SPX_call_midquote_series.index.name = "Contract: (K,T)"
+# print(Augmented_characterized_SPX_call_midquote_series)
+# Augmented_characterized_SPX_call_delta_series = Augmented_SPX_call_delta_series.rename(index=contract_characterization)
+# Augmented_characterized_SPX_call_delta_series.index.name = "Contract: (K,T)"
+# print(Augmented_characterized_SPX_call_delta_series)
+
+
+## Treasury date alignment and missing-day handling
+treasury_dates = list(treasury.index)
+
+### Testing:
+print("\n", Augmented_SPX_call_midquote_series)
+print(Augmented_SPX_call_delta_series)
+# print(treasury.iloc[:,1:])
+print(treasury)
+# print(treasury.columns)
+# print(treasury.shape)
+# print(len(dates))
